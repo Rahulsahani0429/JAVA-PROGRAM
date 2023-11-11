debc0781bbcef9b52118d396551f8fd943e8755c
Rahulsahani0429 <142796638+Rahulsahani0429@users.noreply.github.com>
2023-11-11 20:26:04 +0530

    Add files via upload

---
+++ whileprint.java
@@ -0,0 +1,12 @@
+public class whileprint{
+    public static void main(String[]args){
+        int a=1;
+        while(a<=10){
+
+            System.out.println(a);
+            a++;
+            
+        }
+    }
+    
+}
